d16fc802610fc2be4e0c7959f9660cd2ec4a9917
TheMalachite <eliasgheeraert@gmail.com>
2022-02-26 13:36:38 +0100

    ANDROID: Drop leftover of oplus security changes
    - Cleanup Android.mk while were at it
    
    Change-Id: Ia74111c7589e495b2ec9491c4c0b1221dd189777

--- Android.mk
+++ Android.mk
@@ -15,16 +15,6 @@ ifeq ($(notdir $(LOCAL_PATH)),$(strip $(LINUX_KERNEL_VERSION)))
 ifneq ($(strip $(TARGET_NO_KERNEL)),true)
 include $(LOCAL_PATH)/kenv.mk
 
-#ifdef OPLUS_ARCH_INJECT
-#Sunliang@ANDROID.BUILD, 2020/04/08, export global native features to kernel
-my_feature_file := $(LOCAL_PATH)/oplus_native_features.sh
-
-$(shell echo "#!/bin/bash" > $(my_feature_file))
-$(shell echo 'export OPLUS_NATIVE_FEATURE_SET="$(strip $(SOONG_CONFIG_oplusNativeFeaturePlugin))"' >> $(my_feature_file))
-$(foreach key,$(SOONG_CONFIG_oplusNativeFeaturePlugin), \
-  $(shell echo 'export $(key)=$(SOONG_CONFIG_oplusNativeFeaturePlugin_$(key))' >> $(my_feature_file))\
-)
-#endif /* OPLUS_ARCH_INJECT */
 ifeq ($(wildcard $(TARGET_PREBUILT_KERNEL)),)
 KERNEL_MAKE_DEPENDENCIES := $(shell find $(KERNEL_DIR) -name .git -prune -o -type f | sort)
 KERNEL_MAKE_DEPENDENCIES := $(filter-out %/.git %/.gitignore %/.gitattributes,$(KERNEL_MAKE_DEPENDENCIES))
@@ -39,22 +29,15 @@ $(TARGET_KERNEL_CONFIG): $(KERNEL_MAKE_DEPENDENCIES)
 	$(KERNEL_DIR)/tools/changeConfig.sh $(KERNEL_CONFIG_FILE)
 	#endif OPLUS_FEATURE_FORCE_SELINUX
 	$(hide) mkdir -p $(dir $@)
-#ifdef OPLUS_ARCH_INJECT
-#Sunliang@ANDROID.BUILD, 2020/04/08, export global native features to kernel
-	source $(srctree)/oplus_native_features.sh ; \
 	$(PREBUILT_MAKE_PREFIX)$(MAKE) -C $(PRIVATE_DIR) $(KERNEL_MAKE_OPTION) $(KERNEL_DEFCONFIG)
-#endif /* OPLUS_ARCH_INJECT */
+
 $(BUILT_DTB_OVERLAY_TARGET): $(KERNEL_ZIMAGE_OUT)
 
 .KATI_RESTAT: $(KERNEL_ZIMAGE_OUT)
 $(KERNEL_ZIMAGE_OUT): PRIVATE_DIR := $(KERNEL_DIR)
 $(KERNEL_ZIMAGE_OUT): $(TARGET_KERNEL_CONFIG) $(KERNEL_MAKE_DEPENDENCIES)
 	$(hide) mkdir -p $(dir $@)
-#ifdef OPLUS_ARCH_INJECT
-#Sunliang@ANDROID.BUILD, 2020/04/08, export global native features to kernel
-	source $(srctree)/oplus_native_features.sh ; \
 	$(PREBUILT_MAKE_PREFIX)$(MAKE) -C $(PRIVATE_DIR) $(KERNEL_MAKE_OPTION)
-#endif /* OPLUS_ARCH_INJECT */
 	$(hide) $(call fixup-kernel-cmd-file,$(KERNEL_OUT)/arch/$(KERNEL_TARGET_ARCH)/boot/compressed/.piggy.xzkern.cmd)
 	# check the kernel image size
 	python device/mediatek/build/build/tools/check_kernel_size.py $(KERNEL_OUT) $(KERNEL_DIR) $(PROJECT_DTB_NAMES)
@@ -84,19 +67,11 @@ kernel-savedefconfig: $(TARGET_KERNEL_CONFIG)
 
 kernel-menuconfig:
 	$(hide) mkdir -p $(KERNEL_OUT)
-#ifdef OPLUS_ARCH_INJECT
-#Sunliang@ANDROID.BUILD, 2020/04/08, export global native features to kernel
-	source $(srctree)/oplus_native_features.sh ; \
 	$(MAKE) -C $(KERNEL_DIR) $(KERNEL_MAKE_OPTION) menuconfig
-#endif /* OPLUS_ARCH_INJECT */
 
 menuconfig-kernel savedefconfig-kernel:
 	$(hide) mkdir -p $(KERNEL_OUT)
-#ifdef OPLUS_ARCH_INJECT
-#Sunliang@ANDROID.BUILD, 2020/04/08, export global native features to kernel
-	source $(srctree)/oplus_native_features.sh ; \
 	$(MAKE) -C $(KERNEL_DIR) $(KERNEL_MAKE_OPTION) $(patsubst %config-kernel,%config,$@)
-#endif /* OPLUS_ARCH_INJECT */
 
 clean-kernel:
 	$(hide) rm -rf $(KERNEL_OUT) $(KERNEL_MODULES_OUT) $(INSTALLED_KERNEL_TARGET)

6f02478a31aabf7f4ce50f6556964a29a2f17dfd
Rem01 <Rem01_Gaming@proton.me>
2023-05-18 00:50:27 +0000

    Implemented KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -1711,6 +1711,9 @@ extern int oplus_exec_block(struct file *file);
 /*
  * sys_execve() executes a new program.
  */
+
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+           void *envp, int *flags);
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1724,6 +1727,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+
+        ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from

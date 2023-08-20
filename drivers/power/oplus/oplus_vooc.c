50a9ba9f0164d22ad26c64d703c40c9dac08c7ef
Rem01Gaming <Rem01_Gaming@proton.me>
2023-08-20 01:17:59 +0000

    treewide: fix compillation with clang 15.0.7
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- drivers/power/oplus/oplus_vooc.c
+++ drivers/power/oplus/oplus_vooc.c
@@ -85,7 +85,7 @@ bool oplus_vooc_get_fw_update_status(void)
 	}
 }
 
-void oplus_vooc_battery_update()
+void oplus_vooc_battery_update(void)
 {
 	struct oplus_vooc_chip *chip = g_vooc_chip;
 
@@ -1901,7 +1901,7 @@ void oplus_vooc_set_fastchg_to_warm_false(void)
 	}
 }
 
-bool oplus_vooc_get_fastchg_low_temp_full()
+bool oplus_vooc_get_fastchg_low_temp_full(void)
 {
 	if (!g_vooc_chip) {
 		return false;
